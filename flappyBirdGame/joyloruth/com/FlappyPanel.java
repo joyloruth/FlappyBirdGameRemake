949519e499a1161c6a9ea64981eaf2352bfc5cfc
Joy LoRuth <joyloruth@gmail.com>
2021-06-26 14:46:33 -0500

    added score image

--- flappyBirdGame/joyloruth/com/FlappyPanel.java
+++ flappyBirdGame/joyloruth/com/FlappyPanel.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.Timer;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -17,12 +19,27 @@ import javax.swing.JPanel;
 
 public class FlappyPanel extends JPanel implements KeyListener, ActionListener{
 	
-	final int WIDTH = 525, HEIGHT = 550;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	//private static final long serialVersionUID = 1L;
+	final int WIDTH = 800, HEIGHT = 550;
 	int flappyHeight = HEIGHT/4;
+	final int BARRIER_WIDTH = 90;
 	int velocity  = 0;
 	int acceleration = 4;
 	int shift = 8;
 	int fall = 15;
+	int wallPosition = 200;
+	int shiftWall =  5; 
+	JLabel score = new JLabel();
+	ImageIcon scoreIcon = new ImageIcon("score.png");
+	
+	
 	
 	
 	
@@ -36,13 +53,17 @@ public class FlappyPanel extends JPanel implements KeyListener, ActionListener{
 		
 		
 		new Timer(40, this).start();
+		this.add(score);
+		score.setIcon(scoreIcon);
+		
 	}
 
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		shift = shift + 2; ;
+		shift = shift + 2; 
+		wallPosition = wallPosition - shiftWall;
 		
 		velocity  = velocity + fall;
 		velocity  = velocity + acceleration;
@@ -59,8 +80,15 @@ public class FlappyPanel extends JPanel implements KeyListener, ActionListener{
 	
 	public void drawFlappyBird(Graphics g){
 		g.setColor(Color.yellow);
+		
 		g.fill3DRect(shift, flappyHeight + velocity,25, 25, true);
 	}
+	
+	public void drawBarriers(Graphics g) {
+		g.setColor(Color.green);
+		g.fill3DRect(wallPosition, 0, BARRIER_WIDTH, HEIGHT, true);
+	}
+	
 
 
 	@Override
@@ -69,33 +97,24 @@ public class FlappyPanel extends JPanel implements KeyListener, ActionListener{
 		
 	    if(code == KeyEvent.VK_UP) {velocity = -9; }
 	    if(code == KeyEvent.VK_RIGHT) {shift = 80; }
-	    if(code == KeyEvent.VK_LEFT) {shift = -20; }
+	    if(code == KeyEvent.VK_LEFT) {shift = -1; }
 	}
 	
 	
 
+	
+	
+	
 
 	@Override
 	public void keyReleased(KeyEvent e) {
      
 	}
 
-
 	@Override
 	public void keyTyped(KeyEvent e) {
 		
 	}
-	
-	
-	public void drawBarriers(Graphics g) {
-		g.setColor(Color.green);
-		g.fill3DRect(80, 0, 90, 550, true);
-	}
-	
-
-
-	
-
 	
 
 }
