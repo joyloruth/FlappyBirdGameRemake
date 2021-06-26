f3dc4783dff2165284e5679788a9cd18d5029473
Joy LoRuth <joyloruth@gmail.com>
2021-06-26 15:13:28 -0500

    update

--- flappyBirdGame/joyloruth/com/FrontPanel.java
+++ flappyBirdGame/joyloruth/com/FrontPanel.java
@@ -1,5 +1,7 @@
 package flappyBirdGame.joyloruth.com;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -10,20 +12,28 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class FrontPanel extends JPanel{
-	JButton button = new JButton(new ImageIcon("score.png"));
-	//JButton button = new JButton("Play Game"); 
-	JLabel home = new JLabel();
+	
+	JLabel homeIcon = new JLabel();
+	JButton startButton = new JButton(new ImageIcon("start.png"));
+	JButton optionsButton = new JButton(new ImageIcon("options.png"));
+	
+	
 	
 	FrontPanel(){
+		
 		this.setSize(600,600);
 		this.setVisible(true);
-		this.setBackground(Color.green);
+		this.setBackground(Color.black);
+		//this.setLayout(new CardLayout());
 		
-		this.add(button);
-		this.add(home);
-		ImageIcon icon = new ImageIcon("flappyBird.png");
-		//this.setLayout(new FlowLayout());
-		home.setIcon(icon);
+		this.add(homeIcon);
+		this.add(startButton);
+		this.add(optionsButton);
+		
+		ImageIcon icon = new ImageIcon("menuHeader.png");
+		
+		
+		homeIcon.setIcon(icon);
 		
 		
 	}
