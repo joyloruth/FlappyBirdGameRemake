6f8600e5608017f0f13714a46a095fa39d67e8c0
Joy LoRuth <joyloruth@gmail.com>
2021-06-26 10:52:00 -0500

    added flappyBird favicon image

--- flappyBirdGame/joyloruth/com/Main.java
+++ flappyBirdGame/joyloruth/com/Main.java
@@ -1,5 +1,6 @@
 package flappyBirdGame.joyloruth.com;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 
@@ -8,6 +9,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JFrame frame = new JFrame();
+		ImageIcon icon = new ImageIcon("flappyBird.png");
+		frame.setIconImage(icon.getImage());
 		FlappyPanel flappyPanel = new FlappyPanel();
 		frame.add(flappyPanel);
 		frame.setTitle("FlappyBird Game");
