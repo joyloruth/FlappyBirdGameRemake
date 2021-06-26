0a63da77eec82c206dca970aaecce18458c99c56
Joy LoRuth <joyloruth@gmail.com>
2021-06-26 17:02:58 -0500

    update

--- flappyBirdGame/joyloruth/com/FrontPanel.java
+++ flappyBirdGame/joyloruth/com/FrontPanel.java
@@ -3,6 +3,7 @@ package flappyBirdGame.joyloruth.com;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
@@ -13,29 +14,48 @@ import javax.swing.JPanel;
 
 public class FrontPanel extends JPanel{
 	
-	JLabel homeIcon = new JLabel();
-	JButton startButton = new JButton(new ImageIcon("start.png"));
-	JButton optionsButton = new JButton(new ImageIcon("options.png"));
-	
-	
 	
 	FrontPanel(){
 		
-		this.setSize(600,600);
-		this.setVisible(true);
-		this.setBackground(Color.black);
-		//this.setLayout(new CardLayout());
+		JPanel p = new JPanel();
+		JPanel panel = new JPanel();
+			
+		JLabel homeHeaderLabel = new JLabel();
+		JLabel homeBgLabel = new JLabel();
 		
-		this.add(homeIcon);
+		JButton startButton = new JButton(new ImageIcon("start.png"));
+		JButton optionsButton = new JButton(new ImageIcon("options.png"));
+		
+		ImageIcon homeHeaderImage = new ImageIcon("menuHeader.png");
+		ImageIcon homeBgImage = new ImageIcon("fbbg.png");
+		
+		//this.setBounds(0, 0, 20, 60);
+		panel.setSize(300,300);
+		panel.setVisible(true);
+		panel.setBackground(Color.green);
+		this.setVisible(true);
+		//this.setBackground(Color.blue);
+		this.setLayout(new CardLayout());
+		this.setBackground(Color.red);
+		//labels
+		this.add(homeHeaderLabel);
+		this.add(homeBgLabel);
+		
+		///buttons
 		this.add(startButton);
 		this.add(optionsButton);
 		
-		ImageIcon icon = new ImageIcon("menuHeader.png");
+		homeHeaderLabel.setIcon(homeHeaderImage);
+		homeBgLabel.setIcon(homeBgImage);
+		 
 		
 		
-		homeIcon.setIcon(icon);
 		
 		
 	}
+	
+	
+	}
+	
 
-}
+
