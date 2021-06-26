98bdb0dc651a809f5a771c9b96498940ecc516f9
Joy LoRuth <joyloruth@gmail.com>
2021-06-26 14:46:43 -0500

    update

--- flappyBirdGame/joyloruth/com/Main.java
+++ flappyBirdGame/joyloruth/com/Main.java
@@ -1,5 +1,7 @@
 package flappyBirdGame.joyloruth.com;
 
+import java.util.Scanner;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
@@ -8,17 +10,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		Main main = new Main();
+		 main.menu(num);
+		
+
+	}
+	
+	public void menu(int num) {
+		
 		JFrame frame = new JFrame();
 		ImageIcon icon = new ImageIcon("flappyBird.png");
 		frame.setIconImage(icon.getImage());
 		FlappyPanel flappyPanel = new FlappyPanel();
-		frame.add(flappyPanel);
+		
 		frame.setTitle("FlappyBird Game");
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.setSize(500,500);
+		frame.setSize(800,500);
+		FrontPanel fp = new FrontPanel();
 		
-
+		
+		
+		
+		switch(num) {
+		case 1:
+			frame.add(flappyPanel);
+			break;
+			
+		case 2:
+			frame.add(fp);	
+			break;
+		}
 	}
 
 }
