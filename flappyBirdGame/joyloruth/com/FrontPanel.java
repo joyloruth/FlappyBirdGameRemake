2dbd1493e05aa8fb0f02cff38a11a67f6e0bc8e3
Joy LoRuth <joyloruth@gmail.com>
2021-06-27 07:44:28 -0500

    update

--- flappyBirdGame/joyloruth/com/FrontPanel.java
+++ flappyBirdGame/joyloruth/com/FrontPanel.java
@@ -6,22 +6,31 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class FrontPanel extends JPanel{
-	
+public class FrontPanel extends JPanel implements ActionListener{
+	int count = 0;
+	JLabel demoLabel = new JLabel();
+	final int HEIGHT = 500, WIDTH = 1200;
+
 	
 	FrontPanel(){
+		
+		
 		
 		JPanel p = new JPanel();
 		JPanel panel = new JPanel();
 			
 		JLabel homeHeaderLabel = new JLabel();
 		JLabel homeBgLabel = new JLabel();
+		
 		
 		JButton startButton = new JButton(new ImageIcon("start.png"));
 		JButton optionsButton = new JButton(new ImageIcon("options.png"));
@@ -31,8 +40,9 @@ public class FrontPanel extends JPanel{
 		
 		
 		this.setVisible(true);
+		this.setSize(WIDTH, HEIGHT);
 		
-		this.setLayout(new GridLayout());
+		this.setBorder(BorderFactory.createEmptyBorder(30, 30, 30,30));
 	
 		this.setBackground(Color.black);
 		//labels
@@ -46,15 +56,24 @@ public class FrontPanel extends JPanel{
 		homeHeaderLabel.setIcon(homeHeaderImage);
 		homeBgLabel.setIcon(homeBgImage);
 		
-		int count = 0;
+		startButton.addActionListener(this);
+		
+		this.add(demoLabel);
+		
+		
 		
 		
 		
 		
 	}
-	
-	
-	}
-	
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		count++;
+		demoLabel.setText("Score count: " + count);
+	
+	}
+	
+}
 
--- flappyBirdGame/joyloruth/com/Main.java
+++ flappyBirdGame/joyloruth/com/Main.java
@@ -1,21 +1,69 @@
 package flappyBirdGame.joyloruth.com;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 
 
-public class Main {
+public class Main implements ActionListener {
+	
+	final static int HEIGHT = 500;
+	final static int WIDTH = 1200;
+	
+	static JLabel label = new JLabel();
+	static JButton button = new JButton("Start Game");
+	int count = 0;
+	static GamePanel gamePanel;
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		FrontFrame frame = new FrontFrame();
+		
+		/*Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
 		Main main = new Main();
-		 main.menu(num);
+		 main.menu(num);*/
+		
+		
+		//FRONT PANEL
+		/*JFrame frontFrame = new JFrame();
+		ImageIcon frontIcon = new ImageIcon("flappyBird.png");
+		frontFrame.setIconImage(frontIcon.getImage());		
+		frontFrame.setTitle("FlappyBird Game");
+		frontFrame.setVisible(true);
+		frontFrame.setDefaultCloseOperation(frontFrame.EXIT_ON_CLOSE);
+		frontFrame.setSize(WIDTH, HEIGHT);
+		frontFrame.add(gamePanel);
+		
+		
+		
+		
+		//GAME PANEL
+		JFrame frame = new JFrame();
+		ImageIcon gameIcon = new ImageIcon("flappyBird.png");
+		frame.setIconImage(gameIcon.getImage());
+		
+		
+		frame.setTitle("FlappyBird");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.add(gamePanel); */
+		
+		
+		
+		
+		
+		
 		
 
 	}
@@ -25,28 +73,29 @@ public class Main {
 		JFrame frame = new JFrame();
 		ImageIcon icon = new ImageIcon("flappyBird.png");
 		frame.setIconImage(icon.getImage());
-		FlappyPanel flappyPanel = new FlappyPanel();
+		GamePanel gamePanel = new GamePanel();
 		
 		frame.setTitle("FlappyBird Game");
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.setSize(800,500);
+		frame.setSize(WIDTH, HEIGHT);
 		FrontPanel fp = new FrontPanel();
-		
-		
-		
-		
-		
 		
 		switch(num) {
 		case 1:
-			frame.add(flappyPanel);
+			frame.add(gamePanel);
 			break;
 			
 		case 2:
 			frame.add(fp);	
 			break;
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
